c145d7975f1859fab3e3b454c94019937f15d091
Gavin Wang <gavin8361@gmail.com>
2018-09-05 19:39:40 +1000

    lab07

---
+++ wk1/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+int main (void) {
+	int abs = 4;
+	printf("testing", abs);
+
+	for(i = 0, i < 10, i++) {
+
+		if (n > 5) {
+			printf("I love my abcs")
+
+		}
+
+
+	}
+
+
+	return 0;
+
+}
